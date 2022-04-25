f3e92d015596eab26857a3133357cd1b5eccc03d
Uxio Fuentefria <uxio@gnosis.pm>
2022-04-25 14:46:03 +0200

    Refactor benchmarking

--- benchmark_nodes.py
+++ benchmark_nodes.py
@@ -1,6 +1,7 @@
+import json
 import os
 import time
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,40 +10,51 @@ from dotenv import load_dotenv
 from gnosis.eth import EthereumClient
 
 
+class NodeTimer:
+    def __init__(self, node_version: str):
+        self.node_version = node_version
+        print("Init NodeTimer for", node_version)
+
+    def get_timer(self, test_name: str) -> "Timer":
+        return Timer(self.node_version, test_name)
+
+
 class Timer:
     start: float = 0.0
     elapsed: float = 0.0
-    node_versions: List[str] = []
-    benchmarks: Dict[str, Any] = {}
+    benchmarks: Dict[str, Dict[str, Any]] = {}
 
-    def __init__(self, test_name: str):
+    def __init__(self, node_version: str, test_name: str):
+        self.node_version = node_version
         self.test_name = test_name
 
     def __enter__(self):
-        print(self.test_name)
+        print(self.test_name, ":", "Starting")
         self.start = time.time()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.elapsed = time.time() - self.start
-        self.benchmarks.setdefault(self.test_name, []).append(self.elapsed)
+        self.elapsed = self.get_elapsed()
+        self.benchmarks.setdefault(self.node_version, {})[self.test_name] = self.elapsed
         print(self.test_name, ":", self.elapsed, "seconds")
 
-    @classmethod
-    def plot(cls, node_versions=None, benchmarks=None):
-        node_versions = node_versions or cls.node_versions
-        benchmarks = benchmarks or cls.benchmarks
+    def get_elapsed(self):
+        return time.time() - self.start
 
-        bars1 = []
-        bars2 = []
-        test_names = []
-        for test_name, values in benchmarks.items():
-            bars1.append(values[0])
-            bars2.append(values[1])
-            test_names.append(test_name)
+    @classmethod
+    def plot(cls, benchmarks: Optional[Dict[str, Any]] = None):
+        benchmarks = benchmarks or cls.benchmarks
+        node_versions = list(benchmarks.keys())
+
+        test_names = [
+            test_name for test_name, _ in benchmarks[node_versions[0]].items()
+        ]
+        bars1 = [benchmarks[node_versions[0]][test_name] for test_name in test_names]
+        bars2 = [benchmarks[node_versions[1]][test_name] for test_name in test_names]
+        # TODO Support more bars than 2
 
         x = np.arange(len(test_names))  # the label locations
-        width = 0.25
+        width = 0.2
 
         # Make the plot
         fig, ax = plt.subplots()
@@ -73,42 +85,19 @@ class Timer:
         ax.bar_label(rects2, padding=3)
         fig.subplots_adjust(bottom=0.35, top=0.96)
         fig.set_size_inches(12, 7)
+        # plt.show()
         plt.savefig("plot.png")
 
     @classmethod
     def test_plot(cls):
-        node_versions = [
-            "OpenEthereum//v3.3.5-stable/x86_64-linux-musl/rustc1.59.0",
-            "erigon/2022.04.2/linux-amd64/go1.17.9",
-        ]
-        benchmarks = {
-            "Txs in last block": [0.281780481338501, 0.16505098342895508],
-            "Tx traces in last block": [0.19403791427612305, 3.491976499557495],
-            "Txs in last year block": [0.6601812839508057, 0.3808917999267578],
-            "Tx traces in last year block": [0.8607962131500244, 16.695952653884888],
-            "Erc20 transfers in last 500 blocks": [
-                40.88732099533081,
-                38.98498845100403,
-            ],
-            "Traces for Gnosis Safe v1.1.1 last 200 blocks (30 minutes)": [
-                2.587017774581909,
-                29.74629807472229,
-            ],
-            "Traces for Gnosis Safe v1.1.1 first 100,000 blocks (17 days)": [
-                19.528945446014404,
-                0.7050864696502686,
-            ],
-        }
-        cls.plot(node_versions=node_versions, benchmarks=benchmarks)
+        with open("plot.json", "r") as f:
+            cls.plot(benchmarks=json.load(f))
 
 
-if __name__ == "__main__":
+def perform_benchmark():
     # Timer.test_plot()
     load_dotenv()
-    nodes = [
-        os.getenv("ETHEREUM_NODE_1"),
-        os.getenv("ETHEREUM_NODE_2"),
-    ]
+    nodes = os.getenv("ETHEREUM_NODES").split(",")
 
     ethereum_clients = [
         EthereumClient(node, provider_timeout=300, slow_provider_timeout=300)
@@ -116,49 +105,60 @@ if __name__ == "__main__":
     ]
 
     # Use always same block number for all the benchmarks
-    last_block_number: Optional[int] = None
+    current_block_number = ethereum_clients[0].current_block_number
+    last_block_number = current_block_number - 20  # Use confirmed block
+    print("Last block number:", last_block_number)
     for i, ethereum_client in enumerate(ethereum_clients):
-        Timer.node_versions.append(ethereum_client.w3.clientVersion)
-        print(Timer.node_versions[-1])
+        node_version = ethereum_client.w3.clientVersion
+        node_timer = NodeTimer(node_version)
         last_block = ethereum_client.get_block(
-            last_block_number or "latest", full_transactions=False
+            last_block_number, full_transactions=False
         )
-        last_block_number = last_block["number"]
-        with Timer("Txs in last block") as timer:
+        with node_timer.get_timer("Txs in last block") as timer:
             ethereum_client.get_transactions(last_block["transactions"])
 
-        with Timer("Tx traces in last block") as timer:
+        with node_timer.get_timer("Tx traces in last block") as timer:
             ethereum_client.parity.trace_transactions(last_block["transactions"])
 
+        # Get block from last year
         old_block = ethereum_client.get_block(
             last_block["number"] - 2102400, full_transactions=False
         )
-        with Timer("Txs in last year block") as timer:
+        with node_timer.get_timer("Txs in last year block") as timer:
             ethereum_client.get_transactions(old_block["transactions"])
 
-        with Timer("Tx traces in last year block") as timer:
+        with node_timer.get_timer("Tx traces in last year block") as timer:
             ethereum_client.parity.trace_transactions(old_block["transactions"])
 
-        with Timer("Erc20 transfers in last 500 blocks") as timer:
-            ethereum_client.erc20.get_total_transfer_history(
-                from_block=last_block["number"] - 500
-            )
-
-        with Timer(
-            "Traces for Gnosis Safe v1.1.1 last 200 blocks (30 minutes)"
+        with node_timer.get_timer(
+            "Erc20 transfers in last 500 blocks"  # We don't know the number of events beforehand
         ) as timer:
-            # 0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F was deployed on 9084503
-            print(
-                len(
-                    ethereum_client.parity.trace_filter(
-                        from_block=last_block["number"] - 200,
-                        to_address=["0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F"],
-                    )
+            number_events = len(
+                ethereum_client.erc20.get_total_transfer_history(
+                    from_block=last_block_number - 500, to_block=last_block_number
                 )
             )
+            print(number_events)
+            timer.test_name = (
+                f"Erc20 transfers in last \n500 blocks - {number_events} events"
+            )
 
-        with Timer(
-            "Traces for Gnosis Safe v1.1.1 first 100,000 blocks (17 days)"
+        with node_timer.get_timer(
+            "Traces for Gnosis Safe v1.1.1 last 200 blocks"  # We don't know the number of events before hand
+        ) as timer:
+            # 0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F was deployed on 9084503
+            number_traces = len(
+                ethereum_client.parity.trace_filter(
+                    from_block=last_block_number - 200,
+                    to_block=last_block_number,
+                    to_address=["0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F"],
+                )
+            )
+            print(number_traces)
+            timer.test_name = f"Traces for Gnosis Safe v1.1.1 \nlast 200 blocks (30 minutes)\n{number_traces} traces"
+
+        with node_timer.get_timer(
+            "Traces for Gnosis Safe v1.1.1 \nfirst 100,000 blocks (17 days)\n5 traces"
         ) as timer:
             # 0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F was deployed on 9084503
             from_block = 9084503
@@ -172,8 +172,8 @@ if __name__ == "__main__":
                 )
             )
 
-        with Timer(
-            "Traces for Gnosis Safe v1.1.1 first 200,000 blocks (1 month)"
+        with node_timer.get_timer(
+            "Traces for Gnosis Safe v1.1.1 \nfirst 200,000 blocks (1 month)\n24 traces"
         ) as timer:
             # 0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F was deployed on 9084503
             from_block = 9084503
@@ -188,4 +188,12 @@ if __name__ == "__main__":
             )
 
         print()
+
     Timer.plot()
+
+    with open("plot.json", "w") as f:
+        json.dump(Timer.benchmarks, f, indent=4)
+
+
+if __name__ == "__main__":
+    perform_benchmark()
